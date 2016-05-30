58cbe616eddb5888cecd2180cb8c98d27f543057
Isadora <isadorapossebon@gmail.com>
2016-05-29 21:00:18 -0300

    Relatorios frame

--- LojaAlugueisFesta/src/view/RelatoriosFrame.java
+++ LojaAlugueisFesta/src/view/RelatoriosFrame.java
@@ -5,18 +5,44 @@ import java.awt.BorderLayout;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JFormattedTextField;
+import javax.swing.JComboBox;
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
 
 public class RelatoriosFrame extends JFrame {
 
 	private JPanel contentPane;
 
 	public RelatoriosFrame() {
+		setTitle("Relatórios");
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 504, 432);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblCategoria = new JLabel("Categoria:");
+		lblCategoria.setBounds(20, 25, 72, 16);
+		contentPane.add(lblCategoria);
+		
+		JComboBox categorias_combo_box = new JComboBox();
+		categorias_combo_box.setBounds(104, 21, 130, 27);
+		contentPane.add(categorias_combo_box);
+		
+		JLabel lblData = new JLabel("Data:");
+		lblData.setBounds(276, 25, 40, 16);
+		contentPane.add(lblData);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setBounds(328, 21, 130, 27);
+		contentPane.add(comboBox);
+		
+		JScrollPane resultados_scroll_pane = new JScrollPane();
+		resultados_scroll_pane.setBounds(20, 394, 438, -306);
+		contentPane.add(resultados_scroll_pane);
 	}
-
 }
