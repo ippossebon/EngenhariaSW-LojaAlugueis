bca9b2d790e19786caeefc6f5fd6d8953cea001e
AlquimistaEd <eduardo_butteli@hotmail.com>
2016-06-08 00:56:04 -0300

    básico de relatórios

--- LojaAlugueisFesta/src/controller/RelatoriosController.java
+++ LojaAlugueisFesta/src/controller/RelatoriosController.java
@@ -1,20 +1,44 @@
 package controller;
 
+import java.util.ArrayList;
+
+import javax.swing.table.DefaultTableModel;
+
+import model.Aluguel;
+import model.Cliente;
+import model.Data;
+import model.Funcionario;
+import model.Peca;
+import database.Database;
+import database.DatabaseController;
+
 public class RelatoriosController {
 
 	public RelatoriosController(){
 		
 	}
 	
-	public void getRelatorioLucro(){
+	public void getRelatorioLucro(Data data_inicio, Data data_fim){
 		
 	}
 	
-	public void getClientesBloqueados(){
+	public ArrayList<Cliente> getClientesBloqueados(Data data_inicio, Data data_fim){
 		
+		DatabaseController database_controller = new DatabaseController(Database.getInstance());
+		ArrayList<Cliente> resultado = new ArrayList<Cliente>();
+		
+		for (Cliente c : database_controller.getClientes()){
+			if (c.isBloqueado()){
+				if(){
+					resultado.add(c);
+				}
+			}
+		}
+		
+		return resultado;
 	}
 	
-	public void getAlugueisEmAndamento(){
+	public void getAlugueisEmAndamento(Data data_inicio, Data data_fim){
 		
 	}
 	
