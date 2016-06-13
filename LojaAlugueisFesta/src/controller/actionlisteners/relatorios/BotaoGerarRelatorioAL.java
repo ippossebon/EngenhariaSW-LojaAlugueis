73edf5686a008cf90ddfa7bfaa44d2ad36de5c1c
e-buttelli <eduardo_butteli@hotmail.com>
2016-06-12 22:59:48 -0300

    Esqueleto de BotaoGerarRelatorioAL

--- LojaAlugueisFesta/src/controller/actionlisteners/relatorios/BotaoGerarRelatorioAL.java
+++ LojaAlugueisFesta/src/controller/actionlisteners/relatorios/BotaoGerarRelatorioAL.java
@@ -4,11 +4,20 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 //-----------TESTE--------------------------
 import java.util.ArrayList;
+
+import javax.swing.table.DefaultTableModel;
+
 import model.Data;
+import model.Funcionario;
 import model.Peca;
 import model.Aluguel;
 import model.Cliente;
+import model.RegistroReceita;
+import controller.OperacoesDefaultTableModel;
 import controller.RelatoriosController;
+import database.Database;
+import database.DatabaseController;
+import view.MensagemFrame;
 //-----------TESTE---------------------------
 import view.RelatoriosFrame;
 
@@ -22,9 +31,38 @@ public class BotaoGerarRelatorioAL implements ActionListener{
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+		RelatoriosController relatorios_controller = new RelatoriosController();
 		
-		/*Teste - REMOVER DEPOIS*/
+		try {
+			String data_inicio = this.frame.getData_inicio_text_field().getText();
+			String data_fim = this.frame.getData_fim_text_field().getText();
+			
+			switch((String)this.frame.getFiltro_combo_box().getSelectedItem()){
+			case "Alugueis efetuados":
+				ArrayList<Aluguel> alugueis_efetuados = relatorios_controller.getAlugueisEmAndamento(data_inicio, data_fim);
+				break;
+			case "Quantidade de alugueis":
+				ArrayList<Aluguel> alugueis_efetuados2 = relatorios_controller.getAlugueisEmAndamento(data_inicio, data_fim);
+				int quantidade_alugueis = alugueis_efetuados2.size();
+				break;
+			case "Alugueis por peça":
+				ArrayList<Aluguel> alugueis_por_peca = relatorios_controller.getAlugueisPorPeca(peca, data_inicio, data_fim);
+				break;
+			case "Clientes bloqueados":
+				ArrayList<Cliente> clientes_bloqueados = relatorios_controller.getClientesBloqueados(data_inicio, data_fim);
+				
+				break;
+			case "Lucro":
+				ArrayList<RegistroReceita> receita = relatorios_controller.getRelatorioReceita(data_inicio, data_fim);
+				int total_receita = relatorios_controller.getValorTotalReceita(data_inicio, data_fim);
+				break;
+			}
+		} catch(IllegalArgumentException exception){
+			MensagemFrame msg = new MensagemFrame("Erro ao gerar relatório.");
+			msg.setVisible(true);
+		}
+		
+		/*Teste - REMOVER DEPOIS
 		RelatoriosController relatorios_controller = new RelatoriosController();
 		
 		for (Cliente c: relatorios_controller.getClientesBloqueados("01/01/1992", "01/01/2016")){
@@ -41,7 +79,7 @@ public class BotaoGerarRelatorioAL implements ActionListener{
 			System.out.println("");
 
 		}
-		/*Teste - REMOVER DEPOIS*/
+		Teste - REMOVER DEPOIS*/
 	}
 
 	
